c621c78e503812bf7d954c6b0418d86706da3188
Erick Sutil <ericksutil@gmail.com>
2020-09-21 14:48:07 -0300

    Updates

---
+++ precache-manifest.d56e9a0ae8e928c50a8cc2071a3eb86a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a5398c2051988eb446eaa1dfbdbae8b",
+    "url": "/react-todo/index.html"
+  },
+  {
+    "revision": "acc3c82e92ddd76a959d",
+    "url": "/react-todo/static/css/main.2602e9a2.chunk.css"
+  },
+  {
+    "revision": "d2f621095ad4d400bcb9",
+    "url": "/react-todo/static/js/2.3c3499e0.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/react-todo/static/js/2.3c3499e0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "acc3c82e92ddd76a959d",
+    "url": "/react-todo/static/js/main.29458097.chunk.js"
+  },
+  {
+    "revision": "8b60092a6ae08cc228b0",
+    "url": "/react-todo/static/js/runtime-main.40cbbb06.js"
+  }
+]);
